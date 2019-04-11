0cb2e8ffc12d43e0de75931a6d51f8ea9b57c899
ivate <Fugir2018>
2019-04-11 11:35:27 -0300

    Updates

---
+++ precache-manifest.f6e4b3c5b961b95b68df57ba9ed55dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "891b5547e02ede33a03a",
+    "url": "/react/static/js/main.49fc2426.chunk.js"
+  },
+  {
+    "revision": "2c375ceb382ba6e4d205",
+    "url": "/react/static/js/2.225cfce1.chunk.js"
+  },
+  {
+    "revision": "891b5547e02ede33a03a",
+    "url": "/react/static/css/main.7bf6ce9c.chunk.css"
+  },
+  {
+    "revision": "039a7f5f4ddfa0d3bbad17d2fcb599ad",
+    "url": "/react/index.html"
+  }
+];
